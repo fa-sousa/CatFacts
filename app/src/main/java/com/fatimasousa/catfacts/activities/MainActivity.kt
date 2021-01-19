ce111af3985e6ab7b9593d8a96381cd2ef56b0be
Fatima Ferreira de Sousa <fatimaferreiradesousa@8849-NB.local>
2021-01-19 11:11:33 -0300

    create function to configure factsList

--- app/src/main/java/com/fatimasousa/catfacts/activities/MainActivity.kt
+++ app/src/main/java/com/fatimasousa/catfacts/activities/MainActivity.kt
@@ -3,6 +3,8 @@ package com.fatimasousa.catfacts.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.fatimasousa.catfacts.R
 import com.fatimasousa.catfacts.models.FactsModel
 import com.fatimasousa.catfacts.services.FactsService
@@ -10,6 +12,7 @@ import com.fatimasousa.catfacts.services.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,30 +30,31 @@ class MainActivity : AppCompatActivity() {
         val remote = RetrofitClient.createService(FactsService::class.java)
         val call: Call<List<FactsModel>> = remote.list()
 
-        val response = call.enqueue(object : Callback<List<FactsModel>> {
+        call.enqueue(object : Callback<List<FactsModel>> {
             override fun onFailure(call: Call<List<FactsModel>>, t: Throwable) {
                 val s = t.message
             }
 
             override fun onResponse(call: Call<List<FactsModel>>, response: Response<List<FactsModel>>) {
-                val s = response.body()
-
+                response?.body()?.let {
+                    val facts: List<FactsModel> = it
+                    configureFacts()
+                }
             }
         })
+
     }
 
     private fun goToChooseFragment () {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.frameLayout, ChooseFactFragment())
                 .commit()
-
     }
 
     private fun goToShowFragment () {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.frameLayout, ShowFactFragment())
                 .commit()
-
     }
 
     private fun handleButtonClick() {
@@ -59,12 +63,17 @@ class MainActivity : AppCompatActivity() {
 
         btnHome.setOnClickListener{
             goToChooseFragment()
+
         }
 
         btnGetFact.setOnClickListener{
-            goToShowFragment()
             callApi()
+            goToShowFragment()
         }
     }
 
+    private fun configureFacts() {
+        val facts = FactsModel()
+        facts.text = R.id.txtOnlyFact.toString()
+    }
 }
