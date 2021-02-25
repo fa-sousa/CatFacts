b12fd75ed1b71ebc50b0911626ea241bf643258a
fa-sousa <fsousa292@gmail.com>
2021-02-25 16:21:14 -0300

    configure ViewModel

--- app/src/main/java/com/fatimasousa/catfacts/viewmodel/FactActivityViewModel.kt
+++ app/src/main/java/com/fatimasousa/catfacts/viewmodel/FactActivityViewModel.kt
@@ -3,14 +3,15 @@ package com.fatimasousa.catfacts.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.fatimasousa.catfacts.model.FactModel
 import com.fatimasousa.catfacts.repository.FactActivityRepository
-import com.fatimasousa.catfacts.service.FactsService
 
 class FactActivityViewModel : ViewModel() {
 
-    var servicesLiveData : MutableLiveData<FactsService>? = null
+    var servicesLiveData : MutableLiveData<FactModel>? = null
 
-    fun getUser() : LiveData<FactsService> {
-        servicesLiveData = FactActivityRepository.
+    fun getUser() : LiveData<FactModel>? {
+        servicesLiveData = FactActivityRepository.callApi()
+        return servicesLiveData
     }
 }
