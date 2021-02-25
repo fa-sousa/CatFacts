fdfc21d5ca1034b2bd2e96ee64de1aac6d51d83c
fa-sousa <fsousa292@gmail.com>
2021-02-25 16:11:58 -0300

    add repository

--- app/src/main/java/com/fatimasousa/catfacts/repository/FactActivityRepository.kt
+++ app/src/main/java/com/fatimasousa/catfacts/repository/FactActivityRepository.kt
@@ -1,34 +1,70 @@
 package com.fatimasousa.catfacts.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.fatimasousa.catfacts.model.FactModel
-import com.fatimasousa.catfacts.service.FactsService
 import com.fatimasousa.catfacts.service.RetrofitClient
-import okhttp3.Call
-import okhttp3.Response
-import javax.security.auth.callback.Callback
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 object FactActivityRepository {
 
-    private fun callApi () {
+    val factModel = MutableLiveData<FactModel>()
 
-        val remote = RetrofitClient.createService(FactsService::class.java)
-        val call = remote.list()
+    fun callApi(): MutableLiveData<FactModel> {
 
-        call.enqueue(object : Callback<List<FactModel>> {
-            override fun onFailure(call: Call<List<FactModel>>, t: Throwable) {
-                val s = t.message
+        val call = RetrofitClient.factsService.list()
+
+        call.enqueue(object: Callback<FactModel> {
+            override fun onFailure(call: Call<FactModel>, t: Throwable) {
+                Log.v("DEBUG : ", t.message.toString())
             }
 
-            override fun onResponse(call: Call<List<FactModel>>, response: Response<List<FactModel>>) {
-                val result = response.body()
-                val textToSend = result?.get(2)?.text
+            override fun onResponse(
+                    call: Call<FactModel>,
+                    response: Response<FactModel>
+            ) {
+                Log.v("DEBUG : ", response.body().toString())
 
-                if(textToSend !== null) {
-                    goToShowFragment(textToSend)
-                }
+                val data = response.body()
+
+                val msg = data!!.text
+
+                factModel.value = FactModel()
+
             }
         })
+
+        return factModel
     }
-
-
 }
+
+//----------------------------------------------------------------
+
+//object FactActivityRepository {
+//
+//    private fun callApi () {
+//
+//        val remote = RetrofitClient.createService(FactsService::class.java)
+//        val call = remote.list()
+//
+//        call.enqueue(object : Callback<List<FactModel>> {
+//            override fun onFailure(call: Call<List<FactModel>>, t: Throwable) {
+//                val s = t.message
+//            }
+//
+//            override fun onResponse(call: Call<List<FactModel>>, response: Response<List<FactModel>>) {
+//                val result = response.body()
+//                val textToSend = result?.get(2)?.text
+//
+//                if(textToSend !== null) {
+//                    goToShowFragment(textToSend)
+//                }
+//            }
+//        })
+//    }
+//
+//
+//
+//}
